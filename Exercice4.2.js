743a97d615e2621fbecd37d2e64458bf733ddf49
niyobuhungiro ellen <ellenniyobuhungiro01@gmail.com>
2021-09-17 14:59:28 +0200

    extra

--- Exercice4.2.js
+++ Exercice4.2.js
@@ -1,8 +1,4 @@
-const readlineSync =require("readline-sync");
-
 function rand10() {
     return Math.floor(Math.random() * 10) + 1;
-  }
-  console.log(rand10());
-
-  
+}
+console.log(rand10());
---
+++ Exercice4.3.js
@@ -0,0 +1,14 @@
+function multiRand(n) {
+    for (i = 0; i < n; i++)
+        array.push(Math.floor(Math.random() * 10) + 1);
+    return array[i];
+}
+array = []
+multiRand(5);
+console.log(array);
+
+
+{
+
+
+}
